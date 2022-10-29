068ea1ba979fbc35cd9336fc992200489a30473f
jvdalaglio <jvdalaglio@gmail.com>
2022-10-29 12:04:50 -0300

    componente card configurado

--- src/Components/Layout/Deslogado.style.ts
+++ src/Components/Layout/Deslogado.style.ts
@@ -17,16 +17,32 @@ export const Card = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  padding-top: 72px;
 
 
   background: #FFFFFF;
   border: 1px solid #C5C5C5;
 
   img{
-    margin: 30px;
+    margin-bottom: 30px;
   }
 
   @media only screen and (min-width: 600px) {
     width: 487px;
+    height: 642px;
   }
+
+  h1{
+    width: 71px;
+    height: 25px;
+
+    font-family: 'Questrial';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 24px;
+
+
+    color: #000000;
+  }
+
 `

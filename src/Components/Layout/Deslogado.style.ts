5ef9bd95e3193f57957876a97d4388123b3a597c
jvdalaglio <jvdalaglio@gmail.com>
2022-10-29 11:37:48 -0300

    adicionando background

--- src/Components/Layout/Deslogado.style.ts
+++ src/Components/Layout/Deslogado.style.ts
@@ -1,9 +1,7 @@
 import styled from "styled-components";
 
-export const DeslogadoStyle = styled.div.attrs({
-
-}) `
+export const DeslogadoStyle = styled.div`
   width: 100%;
   height: 100vh;
-  background-image:url();
+  background-image:url(/public/BACKGROUND.svg);
 `

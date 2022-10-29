538d5228c318bd66e853f5f1ee8f6ea893ed3908
jvdalaglio <jvdalaglio@gmail.com>
2022-10-29 10:59:10 -0300

    definindo fonte padrão

--- src/Styles/Global.ts
+++ src/Styles/Global.ts
@@ -1,9 +1,15 @@
 import { createGlobalStyle } from 'styled-components'
 
 export const GlobalStyle = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=Questrial&display=swap');
+
 * {
   padding: 0;
   margin: 0;
   box-sizing: border-box;
+}
+
+html {
+  font-family: 'Questrial';
 }
 `

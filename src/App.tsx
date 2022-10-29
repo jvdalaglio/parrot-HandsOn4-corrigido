611cd3e0b5045b9aaaa706bc7125c40e650c141f
jvdalaglio <jvdalaglio@gmail.com>
2022-10-29 09:34:36 -0300

    iniciando o projeto e instalando pacotes

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  return (
+    <h1>Olá mundo</h1>
+  )
+}
+
+export default App
